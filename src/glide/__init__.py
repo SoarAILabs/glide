7c2b4c39dba3ea13f93acbbd8f306239b1561aa5
Amaan Bilwar <bilwarad@mail.uc.edu>
2025-10-21 19:25:44 -0400

    add: module initialization

---
+++ src/glide/__init__.py
@@ -0,0 +1,8 @@
+"""
+Glide - A Python package with Rust extensions for high-performance operations.
+"""
+
+from .glide import sum_as_string, commit_splitter
+
+__version__ = "0.0.1"
+__all__ = ["sum_as_string", "commit_splitter"]

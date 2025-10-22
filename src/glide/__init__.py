d1d4aa847e1d00923827200b8481ef06e83169b8
Amaan Bilwar <bilwarad@mail.uc.edu>
2025-10-21 21:05:52 -0400

    glide init

--- src/glide/__init__.py
+++ src/glide/__init__.py
@@ -2,7 +2,7 @@
 Glide - A Python package with Rust extensions for high-performance operations.
 """
 
-from .glide import sum_as_string, commit_splitter
+from .glide import gemini_service, helix_service, voyage_service, commit_splitter
 
 __version__ = "0.0.1"
-__all__ = ["sum_as_string", "commit_splitter"]
+__all__ = ["gemini_service", "helix_service", "voyage_service", "commit_splitter"]
